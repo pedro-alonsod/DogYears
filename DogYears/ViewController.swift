4669dd56557bfc7a11eae3048c21f0d81ad78369
pedro-alonsod <pedro.alonsod@gmail.com>
2014-10-28 18:30:18 +0200

    created dog years

--- DogYears/ViewController.swift
+++ DogYears/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dogYearsLabel: UILabel!
+    
+    @IBOutlet weak var convertedDogYearsLabel: UILabel!
+    
+    @IBOutlet weak var dogYearsTextField: UITextField!
+    
+    @IBOutlet weak var dogYearsImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func dogYearsButton(sender: UIButton) {
+        
+        let dogYearsConstant = 7
+        convertedDogYearsLabel.text =  String(dogYearsTextField.text.toInt()! * dogYearsConstant)
+        convertedDogYearsLabel.hidden = false
+        dogYearsTextField.resignFirstResponder()
+        
+        
+    }
 
 }
 
